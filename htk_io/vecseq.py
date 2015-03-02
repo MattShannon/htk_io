56bbd2c3a821df1f3bc727f1be6b334ade81bdf4
Matt Shannon <matt.shannon@cantab.net>
2015-03-02 13:17:57 +0000

    refactor speech_param module and rename to vecseq
    Ignore-this: 90ca942225a8bf6125f5ea28efbee2dd
    
    darcs-hash:20150302131757-3a4db-af2f698a6968a316861a5ccec2e3b51e03eaf4db

---
+++ htk_io/vecseq.py
@@ -0,0 +1,35 @@
+"""Functions for reading and writing raw vector sequence files."""
+
+# Copyright 2014, 2015 Matt Shannon
+
+# This file is part of htk_io.
+# See `License` for details of license and warranty.
+
+import numpy as np
+
+# FIXME : add tests
+
+class VecSeqIo(object):
+    """Reads and writes raw vector sequence files.
+
+    This raw format is used by HTS for speech parameter files (as well as by
+    the Speech Processing Toolkit (SPTK) for lots of purposes).
+    """
+    def __init__(self, vecSize, dtypeFile=np.float32):
+        self.vecSize = vecSize
+        self.dtypeFile = dtypeFile
+
+    def readFile(self, vecSeqFile):
+        """Reads a raw vector sequence file.
+
+        The dtype of the returned numpy array is always the numpy default
+        np.float, which may be 32-bit or 64-bit depending on architecture, etc.
+        """
+        return np.reshape(
+            np.fromfile(vecSeqFile, dtype=self.dtypeFile),
+            (-1, self.vecSize)
+        ).astype(np.float)
+
+    def writeFile(self, vecSeqFile, vecSeq):
+        """Writes a raw vector sequence file."""
+        vecSeq.astype(self.dtypeFile).tofile(vecSeqFile)
